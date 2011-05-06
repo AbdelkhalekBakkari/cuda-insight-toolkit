4bdc98991590264b19afa8f56b97d6942dcf6072
richard.beare <richard.beare@gmail.com>
2011-05-06 05:01:17 +0000

    swapped minimum to new format

--- Examples/Architecture1/src/MinimumImageFilter/CudaMinimumImageFilterKernel.cu
+++ Examples/Architecture1/src/MinimumImageFilter/CudaMinimumImageFilterKernel.cu
@@ -13,32 +13,60 @@
 #include <cutil.h>
 
 template <class T, class S>
-__global__ void MinimumImageKernel(T *output, const S *input, int N)
+__global__ void MinimumImageKernel(S *output, const T *input, int N)
 {
    int idx = blockIdx.x * blockDim.x + threadIdx.x;
    if (idx<N) 
    {
-	   S inputValue = input[idx];
-   	  if (output[idx] > inputValue)
-   	  {
-      	output[idx] = inputValue;
+           S inputValue = input[idx];
+      if (output[idx] > inputValue)
+      {
+         output[idx] = inputValue;
       }
    }
 }
 
-float * MinimumImageKernelFunction(const float* input1, const float* input2, unsigned int N)
+template <class T, class S>
+__global__ void MinimumImageKernel(S *output, const T *input1, const T* input2, int N)
 {
-   float *output;
+   int idx = blockIdx.x * blockDim.x + threadIdx.x;
+   if (idx<N) 
+   {
+   T in1 = input1[idx];
+   T in2 = input2[idx]; 
+   output[idx] = (in1<in2)?in1:in2;
+   }
+}
 
-   output = const_cast<float*>(input1);
-
+template <class T, class S>
+void MinimumImageKernelFunction(const T* input1, const T* input2, S* output, unsigned int N)
+{
    // Compute execution configuration 
    int blockSize = 128;
    int nBlocks = N/blockSize + (N%blockSize == 0?0:1);
 
-   // Call kernel
-   MinimumImageKernel <<< nBlocks, blockSize >>> (output, input2, N);
+   // Call kernal
+   if (output == input1)
+     MinimumImageKernel <<< nBlocks, blockSize >>> (output, input2, N);
+   else
+     MinimumImageKernel <<< nBlocks, blockSize >>> (output, input1, input2, N);
 
    // Return pointer to the output
-   return output;
+   // Return pointer to the output
 }
+
+// versions we wish to compile
+#define THISTYPE float
+template void MinimumImageKernelFunction<THISTYPE, THISTYPE>(const THISTYPE * input1, const THISTYPE * input2, THISTYPE * output, unsigned int N);
+#undef THISTYPE
+#define THISTYPE int
+template void MinimumImageKernelFunction<THISTYPE, THISTYPE>(const THISTYPE * input1, const THISTYPE * input2, THISTYPE *output, unsigned int N);
+#undef THISTYPE
+
+#define THISTYPE short
+template void MinimumImageKernelFunction<THISTYPE, THISTYPE>(const THISTYPE * input1, const THISTYPE * input2, THISTYPE *output, unsigned int N);
+#undef THISTYPE
+
+#define THISTYPE char
+template void MinimumImageKernelFunction<THISTYPE, THISTYPE>(const THISTYPE * input1, const THISTYPE * input2,  THISTYPE *output, unsigned int N);
+#undef THISTYPE
